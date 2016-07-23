705521c594b1e9b921138784d57d043d65b8e479
yzhao343 <yzhao343@gatech.edu>
2016-07-23 00:17:12 -0400

    add me

--- components/me/Me_ctrl.js
+++ components/me/Me_ctrl.js
@@ -2,6 +2,8 @@
     'use strict';
     angular.module('GT_Movie.controller')
     .controller('Me_ctrl', ['$scope', 'socket', 'current_user_info', '$location', function($scope, socket, current_user_info, $location) {
-
+        $scope.goto = function(path) {
+            $location.path(path);
+        }
     }])
 })()
--- components/now_playing/Now_playing_ctrl.js
+++ components/now_playing/Now_playing_ctrl.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     angular.module('GT_Movie.controller')
-    .controller('Now_playing_ctrl', ['$scope', 'socket', 'current_user_info', function($scope, socket, current_user_info) {
+    .controller('Now_playing_ctrl', ['$scope', 'socket', 'current_user_info', '$location', function($scope, socket, current_user_info, $location) {
         $scope.goto_me = function() {
             $location.path('/me');
         }
