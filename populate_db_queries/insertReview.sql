6f6e598b4ab55d21d6831a788ad295d1800f9a35
zzhang391 <lindaz@gatech.edu>
2016-07-23 00:50:07 -0400

    added insertReview.sql

---
+++ populate_db_queries/insertReview.sql
@@ -0,0 +1,115 @@
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('smallleopard364',
+'Best of the Star Trek reboot',
+'I loved this film. It was everything a Star Trek film should be. It was funny, it was fast, it was thrilling.',
+5, 1, 'Star Trek Beyond');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('tinyfish705',
+'Despite it\'s cold setting it\'s a hot mess',
+'Other than the main story-line the movie is filled with unnecessary side stories such as the one where the mammoth Julian is trying to get his future father-in-law Manny to like him.',
+1, 2, 'Ice Age: Collision Course');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('redgorilla703',
+'More Fun than Scary',
+'The villain isn\'t so interesting, apart from her abilities and shortcomings. But the final act is worth the payoff. Maybe not much for the ending. Overall, it\'s a lot of fun than it is scary. For just another trip to the cinema, that\'s all you need to know.',
+4, 3, 'Lights Out');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('bigfish339',
+'Drab Stab',
+'I accept that the film has its champions and I went with the best intentions prepared to give it a fair and unprejudiced viewing but try as I might my eyelids, bereft of matchsticks, refused - or rather declined to remain open for more than ten minutes at a stretch.',
+3, 4, 'Absolutely Fabulous: The Movie');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('orangemeercat994',
+'A Criminally Under-appreciated Film',
+'I felt that Tarzan in 2016 was a brave move for all involved and they pulled it off perfectly and therefore deserve some credit that has not been given to them by movie reviews. It makes me mad how little recognition this film is getting for being the amazing piece of cinema that it truly is.',
+5, 5, 'The Legend of Tarzan');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('lazybird937',
+'Not anywhere nearly as good as Finding Nemo!',
+'If you love Dory, yes you will like the movie. But if Dory all the time is a bit much for you, you will get bored by this fast.',
+2, 6, 'Finding Dory');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('tinyfish559',
+'Bad lead to ending',
+'The film fails to keep us engaged. In the last thirty minutes, the film really starts to drag. The action is not as effective and then builds up to the disappointing emotional climax mentioned earlier. At this point, I just wanted our characters to reach safety, or die already. The very last scene however does work well. In a film filled with brutal action, it was nice to end on a heart-warming moment.',
+3, 7, 'Train to Busan');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('browndog697',
+'Family animation with a traditional feel',
+'The movie is a very family friendly, warm and fun movie. It has less of the tiresome pop-culture gags in it than some other animated releases, keeping the humour within more traditional themes as say an older Disney movie might do which renders it very suitable for anyone of any age.',
+4, 8, 'The Secret Life of Pets');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('silvergorilla475',
+'Most disappointing film I have ever seen?',
+'Very little character / plot development and too many borrowed gags. What was the obsession with the soup? How does a janitor afford to build a nuclear reactor? The original cast members should be ashamed of themselves for appearing in this film.',
+1, 9, 'Ghostbusters');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('whiteladybug301',
+'Oh Dear',
+'Overall I think this film has been a huge disappointment. Every time a film gets given the opportunity to make use of female talent in a positive way, all we seem to get is a massive over reaction by feminist writers to spread hate, sexism and stereotypes.',
+2, 10, 'Ghostbusters');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('beautifulmouse950',
+' Dory and gang are back',
+'This was an amazing film for the whole family to see!',
+5, 11, 'Finding Dory');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('organicwolf498',
+'A Criminally Under-appreciated Film',
+'The movie is a love story and about Tarzan and Jane. The animation or whatever they used for the animals was excellent, There is, of course, deaths, and lots of action. It\'s worth the money to see.',
+4, 12, 'The Legend of Tarzan');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('blackduck351',
+'Another excellent Star Trek which honors the series',
+'They stand on their own, even though they are, in the back of our minds, representing icons of the Star Trek franchise from the original cast of the 1960s. STAR TREK BEYOND presents a glimpse of the future which is coming - it\'s just a matter of time and technology.',
+5, 13, 'Star Trek Beyond');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('blackduck351',
+'Really Funny Film',
+'I think each character got the chance to shine in this one, which had been lacking in the previous two installments. But comedy wise, I actually think this could be smarter and funnier in its humor to Secret Life Of Pets, which was really funny too- but more sight gags.',
+4, 14, 'Ice Age: Collision Course');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('blackduck351',
+'Yeah about this movie!!',
+'They went over the top in about 90% of the movie, don\'t get me wrong there were a few parts that had me laughing hard but that was not enough to give it more than a 5- acting was mediocre.',
+3, 15, 'Mike and Dave Need Wedding Dates');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('whiteladybug301',
+'Movie about Corrupted Government',
+'Purge election year strongly served on what some government official will do to ensure their win in an election and how others will put faith in the voters.',
+4, 16, 'The Purge: Election Year');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('silvergorilla475',
+'Hilarious with a touch of sweetness',
+'I rarely see comedies in my town because of boring crowds who don\'t make a peep at the funniest parts. I think This film has a very wide appeal and I highly recommend it if you\'re looking for a fun night out.',
+4, 17, 'Central Intelligence');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('browndog697',
+'A bit disappointing',
+'There\'s even a scene where the BFG does nothing but hop around and giggle. Why, you may ask? I don\'t know. The film definitely doesn\'t know. So, in summary, no one knows. I hate to say this, but, this film\'s a mess.',
+3, 18, 'The BFG');
+ 
+INSERT INTO REVIEW (User, Title, Comment, Rating, Review_id, Mtitle)
+VALUES ('orangemeercat994',
+'Some cool effects, but...',
+'Every plot twist that wasn\'t blatantly predictable was helpfully telegraphed to make it so; no time was spent (or, I suppose in the creators\' view "wasted") on character development beyond the most absolutely superficial; the acting warranted a disinterested "meh;" and the obvious sequel hook ending was really presumptuous',
+2, 19, 'Independence Day: Resurgence');
+ 
+
