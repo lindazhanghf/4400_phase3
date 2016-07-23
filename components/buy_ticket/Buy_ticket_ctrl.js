0bb3d4326cfb36e5df05bb1aa978cb1bcfc6fc83
zzhang391 <lindaz@gatech.edu>
2016-07-23 17:00:21 -0400

    Update buy ticket with not fixed

--- components/buy_ticket/Buy_ticket_ctrl.js
+++ components/buy_ticket/Buy_ticket_ctrl.js
@@ -2,6 +2,8 @@
     'use strict';
     angular.module('GT_Movie.controller')
     .controller('Buy_ticket_ctrl', ['$scope', 'socket', 'current_user_info', '$location', function($scope, socket, current_user_info, $location) {
+        $scope.select = {};
+        $scope.select.Tid = 1;
         socket.emit('get_preferred_theaters', current_user_info.user.Username);
         socket.on('preferred_theaters', function(data) {
             $scope.preferred_theaters = data;
@@ -10,12 +12,13 @@
         function goto(path) {
             $location.path(path);
         }
-        function select_theater(theater_id) {
-            current_user_info.ticket.Tid = theater_id;
-            console.log('Buy ticket: ' + current_user_info.user.Username + " select ", + theater_id.toString())
+        $scope.select_theater = function() {
+            current_user_info.ticket = {};
+            current_user_info.ticket.Tid = select.Tid;
+            console.log('Buy ticket: ' + current_user_info.user.Username + " select " + current_user_info.ticket.Tid)
             $location.path('/select_time')
         }
-        function search_theater(search_word) {
+        $scope.search_theater = function(search_word) {
             socket.emit('search_theater', search_word)
             console.log('Buy ticket: ' + current_user_info.user.Username + ' search ' + search_word )
         }
