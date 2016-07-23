89d09101d4516a03e611ba44a5adf0ac9593c8cf
zzhang391 <lindaz@gatech.edu>
2016-07-23 00:53:33 -0400

    added insert_Prefers.sql

---
+++ populate_db_queries/insert_Prefers.sql
@@ -0,0 +1,30 @@
+INSERT INTO PREFERS (User, Tid)
+VALUES ('orangemeercat994', 1);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('orangemeercat994', 2);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('browndog697', 3);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('browndog697', 4);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('silvergorilla475', 5);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('silvergorilla475', 6);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('blackduck351', 2);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('blackduck351', 3);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('whiteladybug301', 4);
+ 
+INSERT INTO PREFERS (User, Tid)
+VALUES ('whiteladybug301', 5);
+
